187757f790c68cd33b9bc1a9e56f82b431b7e0dc
sebprt <sebastien.parrat@orange.fr>
2021-03-24 16:59:12 +0100

    updated csv extrator and loader with new params

--- src/CSV/Safe/Extractor.php
+++ src/CSV/Safe/Extractor.php
@@ -4,19 +4,27 @@ namespace Kiboko\Component\Flow\Spreadsheet\CSV\Safe;
 
 use Box\Spout\Common\Entity\Row;
 use Box\Spout\Reader\CSV\Reader;
-use Box\Spout\Reader\Exception\ReaderNotOpenedException;
 use Kiboko\Component\Flow\Spreadsheet\Sheet;
 use Kiboko\Contract\Pipeline\ExtractorInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 
 class Extractor implements ExtractorInterface
 {
-    /**
-     * @throws ReaderNotOpenedException
-     */
+    private LoggerInterface $logger;
+
     public function __construct(
         private Reader $reader,
-        private int $skipLines = 0
+        private int $skipLines = 0,
+        private string $delimiter = ',',
+        private string $enclosure = '"',
+        private string $encoding = 'UTF-8',
+        ?LoggerInterface $logger = null
     ) {
+        $this->logger = $logger ?? new NullLogger();
+        $this->reader->setFieldDelimiter($this->delimiter);
+        $this->reader->setFieldEnclosure($this->enclosure);
+        $this->reader->setEncoding($this->encoding);
     }
 
     public function extract(): iterable
