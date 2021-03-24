54f9910c269e19f15dab3407cd380b7ff2f892f6
sebprt <sebastien.parrat@orange.fr>
2021-03-24 16:59:12 +0100

    updated csv extrator and loader with new params

--- src/CSV/Safe/Loader.php
+++ src/CSV/Safe/Loader.php
@@ -11,11 +11,22 @@ use Kiboko\Component\Flow\Spreadsheet\Sheet;
 use Kiboko\Contract\Bucket\ResultBucketInterface;
 use Kiboko\Contract\Pipeline\FlushableInterface;
 use Kiboko\Contract\Pipeline\LoaderInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 
 class Loader implements LoaderInterface, FlushableInterface
 {
-    public function __construct(private Writer $writer)
-    {
+    private LoggerInterface $logger;
+
+    public function __construct(
+        private Writer $writer,
+        private string $delimiter = ',',
+        private string $enclosure = '"',
+        ?LoggerInterface $logger = null
+    ) {
+        $this->logger = $logger ?? new NullLogger();
+        $this->writer->setFieldDelimiter($this->delimiter);
+        $this->writer->setFieldEnclosure($this->enclosure);
     }
 
     public function load(): \Generator
